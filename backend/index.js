5fd0a4fb5444c30e449437fd34d0a2d536126c4e
pravin <pravin@algoware.in>
2026-08-06 18:49:15 +0530

    Fix nested git repository and backed files

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,35 @@
+global.crypto = require('crypto'); 
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const config = require('./config.json');
+const connectDB = require('./config/db');
+//routes
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+const cartRoutes = require('./routes/cartRoutes');
+const productRouter = require('./routes/product');
+const addressRoute = require('./routes/addressRoute');
+
+//image route
+const pathData = require("./routes/product");
+const path = require("path");
+
 const app = express();
-
-app.use(cors());
 app.use(express.json());
-/*
-const authRouter = require('./routes/auth')
-const productRouter = require('./routes/product')
-const cartRouter = require('./routes/cart')
-const userRouter = require('./routes/user')
+connectDB();
+app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost', credentials: true }));
 
-app.use('/api/v1/auth', authRouter);
+app.use("/api/v1",pathData);//direct image access   
+//image url
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+//login and register routes
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/cart', cartRoutes);
 app.use('/api/v1/products', productRouter);
-app.use('/api/v1/cart', cartRouter);
-app.use('/api/v1/user', userRouter);
+app.use('/api/v1/user', addressRoute);
 
-const port = process.env.PORT || config.port
-app.listen(port, () => {
-    console.log(`Foods Backend running at port ${port}`);
-});
-*/
 
+const PORT = process.env.PORT || 8082;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
